59baa950efe0dcbc525b3c5bd2245f49dcb03970
ts <ts@slickjump.com>
2020-09-17 17:30:53 +0300

    whitelist for users added

--- camelot_bot.py
+++ camelot_bot.py
@@ -10,24 +10,28 @@ bot = telebot.TeleBot(TOKEN)
 keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
 keyboard.row('Расписание')
 
+users = [78623899]
+
 
 @bot.message_handler(commands=['start'])
 def start(message):
-	bot.send_message(message.chat.id, 'Привет! Выбери необходимое действие', reply_markup=keyboard)
+	if message.from_user.id in users:
+		bot.send_message(message.chat.id, 'Привет! Выбери необходимое действие', reply_markup=keyboard)
 
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-	if message.text.lower() == 'привет':
-		bot.send_message(message.chat.id, 'Привет')
-	elif message.text.lower() == 'как дела?':
-		bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAIDGV8_yLbpXxAEhPKKxrHMjDRX-pK9AAKgAAP3AsgPw0cdAaCbwBobBA')
-	elif message.text.lower() == 'пока':
-		bot.send_message(message.chat.id, 'Пока!')
-	elif message.text.lower() == 'расписание':
-		bot.send_message(message.chat.id, open('schedule.txt').read())
-	else:
-		bot.send_message(message.chat.id, 'Фигню написал, попробуй еще раз.')
+	if message.from_user.id in users:
+		if message.text.lower() == 'привет':
+			bot.send_message(message.chat.id, 'Привет')
+		elif message.text.lower() == 'как дела?':
+			bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAIDGV8_yLbpXxAEhPKKxrHMjDRX-pK9AAKgAAP3AsgPw0cdAaCbwBobBA')
+		elif message.text.lower() == 'пока':
+			bot.send_message(message.chat.id, 'Пока!')
+		elif message.text.lower() == 'расписание':
+			bot.send_message(message.chat.id, open('schedule.txt').read())
+		else:
+			bot.send_message(message.chat.id, 'Фигню написал, попробуй еще раз.')
 
 
 #@bot.message_handler(content_types=['sticker'])
