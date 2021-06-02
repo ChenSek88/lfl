92070d94354168898e88de76626feaed3883115b
ts <ts@slickjump.com>
2021-06-02 17:05:48 +0300

    games results added

--- generate_html.py
+++ generate_html.py
@@ -18,9 +18,9 @@ TEMP_DIR = HOME_DIR + 'temp_tables/'
 
 tournament_stats_url = "https://lfl.ru/?ajax=1&method=tournament_stats_table&tournament_id=7190&club_id=2356&season_id=41"
 tournament_calendar_url= "https://lfl.ru/?ajax=1&method=tournament_calendar_table&tournament_id=7190&club_id=2356&season_id=41"
-tournament_results_url = "https://lfl.ru/?ajax=1&method=tournament_resault_table&tournament_id=7190&club_id=2356&season_id=41"
 players_stats_url = "https://lfl.ru/?ajax=1&method=tournament_squads_table&tournament_id=7190&club_id=2356&season_id=41"
 disqualifications_url= "https://lfl.ru/?ajax=1&mode=new_format&method=tournament_disqualifications_table&tournament_id=7190&club_id=2356&season_id=41"
+games_results_url = "https://lfl.ru/?ajax=1&method=tournament_resault_table&tournament_id=0&club_id=2356&season_id=41"
 user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36'
 
 up = ['pos_1', 'pos_2']
@@ -53,7 +53,7 @@ def get_table(file, url):
 get_table(TEMP_DIR + 'tournament_table', tournament_stats_url)
 get_table(TEMP_DIR + 'calendar_table', tournament_calendar_url)
 get_table(TEMP_DIR + 'players_table', players_stats_url)
-get_table(TEMP_DIR + 'results_table', tournament_results_url)
+get_table(TEMP_DIR + 'games_results_table', games_results_url)
 get_table(TEMP_DIR + 'disqual_table', disqualifications_url)
 
 
@@ -292,6 +292,75 @@ def disqual_table():
 			return tour_list
 
 
+
+#generate games results table
+#@catch_exception
+def games_results_table():
+	with open(TEMP_DIR + 'games_results_table') as table:
+		soup = BeautifulSoup(table, 'lxml')
+		if soup.find('tbody'):
+			table = soup.find('tbody')
+			tr = table.find_all('tr')
+			tour_list = ''
+
+
+			def tour_list_desktop():
+				return """<th class="col-1 d-sm-none d-md-block d-none">%s</th>
+							<td class="col-3 d-sm-none d-md-block d-none text-right">%s<img class="club-logo" src="%s"></td>
+							<td class="col-1 d-sm-none d-md-block d-none"><span class="result">%s</span></td>
+							<td class="col-3 d-sm-none d-md-block d-none"><img class="club-logo" src="%s">%s</td>
+							<td class="col-4 d-sm-none d-md-block d-none"><span class="place">%s</span></td>
+						""" %(tour, host, path_to_host_img, result, path_to_guest_img, guest, date)
+
+
+			def tour_list_tablet():
+				return """<td class="col-2 d-none d-sm-block d-md-none text-center"><img class="club-logo-big" src="%s"></td>
+							<td class="col-8 d-none d-sm-block d-md-none text-center">
+							<span class="badge badge-success">Тур %s</span><br>
+							<b>%s %s %s</b><br>
+							<span class="place">%s</span></td>
+							<td class="col-2 d-none d-sm-block d-md-none text-center"><img class="club-logo-big" src="%s"><br></td>
+						""" %(path_to_host_img, tour, host, result, guest, date, path_to_guest_img)
+
+
+			def tour_list_mobile():
+				return """<td class="col-12 d-block d-sm-none text-center">
+							<span class="badge badge-success">Тур %s</span><br>
+							<img class="club-logo-small" src="%s"><b>%s %s %s</b><img class="club-logo-small" src="%s"><br>
+							<span class="place">%s</span></td>
+						"""%(tour, path_to_host_img, host, result, guest, path_to_guest_img, date)
+
+
+			for i in tr:
+				find_all_img = i.find_all('img')
+				tour = i.find_all('td')[0].get_text()
+				#date = i.find_all('td')[1].get_text()
+				#time = i.find_all('td')[2].get_text()
+				date = (i.find_all('td')[1].get_text() + ' ' + i.find_all('td')[7].get_text() + ', ' + i.find_all('td')[2].get_text())
+				host = i.find_all('td')[3].get_text()
+				result = i.find_all('td')[4].get_text()
+				guest = i.find_all('td')[5].get_text()
+				host_img = i.find_all('img')[0]['src']
+				guest_img = i.find_all('img')[1]['src']
+				host_filename = host_img.split("/")[-1]
+				guest_filename = guest_img.split("/")[-1]
+				path_to_host_img = '/images/' + host_filename
+				path_to_guest_img = '/images/' + guest_filename
+				outpath = os.path.join(HOME_DIR + 'images/', host_filename)
+				outpath2 = os.path.join(HOME_DIR + 'images/', guest_filename)
+				tour_list += """<tr class="row">%s%s%s</tr>"""%(tour_list_desktop(), tour_list_tablet(), tour_list_mobile())
+				if os.path.exists(outpath) and os.path.exists(outpath2):
+					pass
+				else:
+					opener = urllib.request.build_opener()
+					opener.addheaders = [('User-Agent', user_agent)]
+					urllib.request.install_opener(opener)
+					urllib.request.urlretrieve(host_img, outpath)
+					urllib.request.urlretrieve(guest_img, outpath2)
+			return tour_list
+
+
+
 html = open(HOME_DIR + 'template.html').read()
 template = Template(html)
 
@@ -299,10 +368,12 @@ tournament = tournament_table()
 calendar = calendar_table()
 players = players_table()
 disqual = disqual_table()
+results = games_results_table()
+
 
 with open(HOME_DIR + "index.html", "w") as index:
 	index.write(template.render(tournament_table=tournament, calendar_table=calendar,
-		players_table=players, disqual_table=disqual))
+		players_table=players, disqual_table=disqual, games_results_table=results))
 	print('index.html updated successfully!' + '	' +  str(datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
 
 shutil.rmtree(HOME_DIR + 'temp_tables')
