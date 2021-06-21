bebc8c8afdc649652f907796ff18b664e87010de
ts <ts@slickjump.com>
2021-06-21 12:30:53 +0300

    unusable code removed

--- camelot_bot.py
+++ camelot_bot.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-#import telebot
-#from telebot import types
 from datetime import datetime
 import requests
 from config import config
@@ -10,7 +7,6 @@ import random
 
 
 TOKEN = config.get('TOKEN', 'token')
-greeting = []
 
 
 url = 'https://api.telegram.org/bot'
@@ -73,65 +69,3 @@ for hash, sch in schedule.items():
 
 
 print('The work is completed' + '	' +  str(datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
-'''def make_hash():
-	date = open('schedule.txt').readlines()[1].strip() + ':00'
-	now = datetime.now()
-	strp_date = datetime.strptime(date, "%d.%m.%Y %H:%M:%S")
-	diff = ((strp_date - now).total_seconds())/60
-	hours, minutes = divmod(diff, 60)
-	if hours <= 24:
-		print(hashlib.md5(second_message.encode('utf-8')).hexdigest())
-		return hashlib.md5(second_message.encode('utf-8')).hexdigest()
-		#days, hours = divmod(hours, 24)
-		#return("%02dч %02dмин"% (hours, minutes))
-	elif hours <= 3:
-		print(hashlib.md5(third_message.encode('utf-8')).hexdigest())
-		return hashlib.md5(third_message.encode('utf-8')).hexdigest()
-		#return("%02dч %02dмин"% (hours, minutes))
-
-
-with open('hash_list.txt', 'a') as hash_list:
-	if make_hash() in temp_list:
-		print('Сообщение было отправлено')
-	else:
-		send_message(second_message) or send_message(third_message)
-		hash_list.write("\n" + make_hash())'''
-
-
-'''bot = telebot.TeleBot(TOKEN)
-keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-keyboard.row('Расписание')
-
-
-@bot.message_handler(commands=['start'])
-def start(message):
-	if message.from_user.id in users:
-		bot.send_message(message.chat.id, 'Привет! Выбери необходимое действие', reply_markup=keyboard)
-
-
-@bot.message_handler(content_types=['text'])
-def send_text(message):
-	if message.from_user.id in users:
-		if message.text.lower() == 'привет':
-			bot.send_message(message.chat.id, 'Привет')
-		elif message.text.lower() == 'как дела?':
-			bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAIDGV8_yLbpXxAEhPKKxrHMjDRX-pK9AAKgAAP3AsgPw0cdAaCbwBobBA')
-		elif message.text.lower() == 'пока':
-			bot.send_message(message.chat.id, 'Пока!')
-		elif message.text.lower() == 'расписание':
-			bot.send_message(message.chat.id, open('schedule.txt').read())
-		else:
-			bot.send_message(message.chat.id, 'Фигню написал, попробуй еще раз.')
-
-
-@bot.message_handler(content_types=['sticker'])
-def sticker_id(message):
-	print(message)
-
-@bot.message_handler(commands=['start'])
-def send_text(message):
-	if message.chat.id in chat:
-		bot.send_message(message.chat.id, open('schedule.txt').read())
-
-
-bot.polling()'''
