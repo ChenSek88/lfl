45342c03fb83bf56d0a6881d57ac486e6c0375a2
ts <ts@slickjump.com>
2022-02-03 15:13:09 +0300

    new season added

--- generate_html.py
+++ generate_html.py
@@ -16,11 +16,11 @@ HOME_DIR = config.get('home_dir', 'path')
 os.mkdir(HOME_DIR + 'temp_tables/')
 TEMP_DIR = HOME_DIR + 'temp_tables/'
 
-tournament_stats_url = "https://lfl.ru/?ajax=1&method=tournament_stats_table&tournament_id=7190&club_id=2356&season_id=41"
-tournament_calendar_url= "https://lfl.ru/?ajax=1&method=tournament_calendar_table&tournament_id=0&club_id=2356&season_id=41"
-players_stats_url = "https://lfl.ru/?ajax=1&method=tournament_squads_table&tournament_id=7190&club_id=2356&season_id=41"
-disqualifications_url= "https://lfl.ru/?ajax=1&mode=new_format&method=tournament_disqualifications_table&tournament_id=7190&club_id=2356&season_id=41"
-games_results_url = "https://lfl.ru/?ajax=1&method=tournament_resault_table&tournament_id=0&club_id=2356&season_id=41"
+tournament_stats_url = "https://lfl.ru/?ajax=1&method=tournament_stats_table&tournament_id=0&club_id=2356&season_id=43"
+tournament_calendar_url= "https://lfl.ru/?ajax=1&method=tournament_calendar_table&tournament_id=0&club_id=2356&season_id=43"
+players_stats_url = "https://lfl.ru/?ajax=1&method=tournament_squads_table&tournament_id=13682&club_id=2356&season_id=43"
+disqualifications_url= "https://lfl.ru/?ajax=1&mode=new_format&method=tournament_disqualifications_table&tournament_id=0&club_id=2356&season_id=43"
+games_results_url = "https://lfl.ru/?ajax=1&method=tournament_resault_table&tournament_id=0&club_id=2356&season_id=43"
 user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36'
 
 up = ['pos_1', 'pos_2']
@@ -205,61 +205,67 @@ def calendar_table():
 
 
 #generate players table
-@catch_exception
+#@catch_exception
 def players_table():
 	with open(TEMP_DIR + 'players_table') as table:
 		soup = BeautifulSoup(table, 'lxml')
-		table = soup.find('tbody')
+		if soup.find('tbody'):
+			table = soup.find('tbody')
 
-		tr = table.find_all('tr')
-		for i in tr:
-			td = i.find_all('td')
-			i['class'] = 'table-light'
-			del i['data-division-id']
-			del i['data-tournament-id']
+			tr = table.find_all('tr')
+			for i in tr:
+				td = i.find_all('td')
+				i['class'] = 'table-light'
+				del i['data-division-id']
+				del i['data-tournament-id']
 
 
-		tour_header = table.find_all(attrs={"class":"tour-header"})
-		for i in tour_header:
-			i['class'] = 'amplua text-center font-weight-bold'
+			tour_header = table.find_all(attrs={"class":"tour-header"})
+			for i in tour_header:
+				i['class'] = 'amplua text-center font-weight-bold'
 
-		players = table.find_all(attrs={"class":"player"})		
-		for i in players:
-			i['href'] = 'https://lfl.ru' + i['href']
-			i['class'] = 'text-body'
-			i['target'] = '_blank'
+			players = table.find_all(attrs={"class":"player"})		
+			for i in players:
+				i['href'] = 'https://lfl.ru' + i['href']
+				i['class'] = 'text-body'
+				i['target'] = '_blank'
 
-		images = table.find_all(attrs={"class":"usr-image_link"})
-		for img in images:
-			img['href'] = 'https://lfl.ru' + img['href']
-			img['target'] = '_blank'
-			style = img['style']
-			image = re.search("http.*[)]", style)
-			image_url = style[image.start():image.end()-1]
-			image_name = image_url.split("/")[-1]
-			img['style'] = 'background: url(images/%s) no-repeat center center' %image_name
-			img['class'] = (img['class'] + ['pr-1'])
-			outpath = os.path.join(HOME_DIR + 'images/', image_name)
-			if os.path.exists(outpath):
-				pass
-			else:
-				opener = urllib.request.build_opener()
-				opener.addheaders = [('User-Agent', user_agent)]
-				urllib.request.install_opener(opener)
-				urllib.request.urlretrieve(image_url, outpath)
+			images = table.find_all(attrs={"class":"usr-image_link"})
+			for img in images:
+				img['href'] = 'https://lfl.ru' + img['href']
+				img['target'] = '_blank'
+				style = img['style']
+				image = re.search("http.*[)]", style)
+				image_url = style[image.start():image.end()-1]
+				image_name = image_url.split("/")[-1]
+				img['style'] = 'background: url(images/%s) no-repeat center center' %image_name
+				img['class'] = (img['class'] + ['pr-1'])
+				outpath = os.path.join(HOME_DIR + 'images/', image_name)
+				if os.path.exists(outpath):
+					pass
+				else:
+					opener = urllib.request.build_opener()
+					opener.addheaders = [('User-Agent', user_agent)]
+					urllib.request.install_opener(opener)
+					urllib.request.urlretrieve(image_url, outpath)
 
-		td = table.find_all('td')
-		for i in td:
-			if not i.has_attr('class'):
-				i['class'] = 'text-center align-middle'
-			span = i.find_all('span')
-			for s in span:
-				s.extract()	
-		return table
+			td = table.find_all('td')
+			for i in td:
+				if not i.has_attr('class'):
+					i['class'] = 'text-center align-middle'
+				span = i.find_all('span')
+				for s in span:
+					s.extract()	
+			return table
+		else:
+			table = soup.find_all('div')
+			table[0]['class'] == ['empty-list']
+			tour_list = """<tr class="row"> </tr>"""
+			return tour_list
 
 
 #generate disqual table
-@catch_exception
+#@catch_exception
 def disqual_table():
 	with open(TEMP_DIR + 'disqual_table') as table:
 		soup = BeautifulSoup(table, 'lxml')
